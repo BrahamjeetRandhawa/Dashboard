1905a1f02f69dabad34ca4e871568b7489aaa2ed
Braham <Braham0306@hotmail.com>
2025-04-23 17:02:53 +0200

    Update

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 import Top from './Top'; // Import the Top component
 import Sidebar from './Sidebar'; // Import the Sidebar component
+import Filter from './Filter'; // Import the Filter component
 
 function App() {
   return (
     <div className="App-container">
       <Top className="Topbar" />
+      <Filter className="Filter" />
       <Sidebar className="Sidebar" />
     </div>
   );
---
+++ dashboard/src/Filter.js
@@ -0,0 +1,17 @@
+import './Filterstyle.css';
+
+
+function Filter() {
+    return (
+        <div className="Filter">
+            <label htmlFor="filter">Filter:</label>
+            <button type="button">Apply Filter</button>
+            <button type="button">Clear Filter</button>
+            <button type="button">Save Filter</button>
+            <button type="button">Load Filter</button>
+            <button type="button">Export Filter</button>
+        </div>
+    );
+}
+
+export default Filter;
