108b8cd650a836a081990f6e6c9dd501e011bcb5
Braham <Braham0306@hotmail.com>
2025-04-24 16:33:52 +0200

    Topbar update

--- dashboard/src/Top.js
+++ dashboard/src/Top.js
@@ -3,8 +3,12 @@ import './Topstyle.css';
 
 function Top() {
   return (
-    <div className="Topbars">
+    <div className="Topbar">
       <h1>EMEA banking</h1>
+      <div className="Topbar-left">
+        <h2>DELIVERY DASHBOARD-FY2526</h2>
+        <p>OUTCOME BASED METRIC IMPLEMENTATION</p>
+      </div>
     </div>
   );
 }
