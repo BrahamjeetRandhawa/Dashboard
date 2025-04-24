7e2af5605ab8ff16757064b016d34bd95543cdfb
Braham <Braham0306@hotmail.com>
2025-04-24 16:01:43 +0200

    Sidebar name update

--- dashboard/src/Sidebar.js
+++ dashboard/src/Sidebar.js
@@ -4,21 +4,25 @@ import './Sidebarstyle.css';
 
 function Sidebar() {
   return (
-    <div className="Sidebar">
+    <nav className="Sidebar">
       <ul className="Sidebar__menu">
-        <li className="Sidebar__menu-item"><a href="#financial">Financial</a></li>
-        <li className="Sidebar__menu-item"><a href="#about">About</a></li>
-        <li className="Sidebar__menu-item"><a href="#contact">Contact</a></li>
-        <li className="Sidebar__menu-item"><a href="#services">Services</a></li>
-        <li className="Sidebar__menu-item"><a href="#team">Team</a></li>
-        <li className="Sidebar__menu-item"><a href="#testimonials">Testimonials</a></li>
-        <li className="Sidebar__menu-item"><a href="#blog">Blog</a></li>
-        <li className="Sidebar__menu-item"><a href="#faq">FAQ</a></li>
-        <li className="Sidebar__menu-item"><a href="#privacy">Privacy Policy</a></li>
-        <li className="Sidebar__menu-item"><a href="#terms">Terms of Service</a></li>
-        <li className="Sidebar__menu-item"><a href="#careers">Careers</a></li>
+        <li className="Sidebar__menu-item"><a href="#FINANCIAL">FINANCIAL</a></li>
+        <li className="Sidebar__menu-item"><a href="#DEMAND VIEW">DEMAND VIEW</a></li>
+        <li className="Sidebar__menu-item"><a href="#FULFILMENT VIEW">FULFILMENT VIEW</a></li>
+        <li className="Sidebar__menu-item"><a href="#CLIENT PARTNER FOCUS">CLIENT PARTNER FOCUS</a></li>
+        <li className="Sidebar__menu-item"><a href="#THOUGHT LEADERSHIP DEMONSTRATION">THOUGHT LEADERSHIP DEMONSTRATION</a></li>
+        <li className="Sidebar__menu-item"><a href="#NORTH STAR POD">NORTH STAR POD</a></li>
+        <li className="Sidebar__menu-item"><a href="#GTM IMPROVEMENT FOCUS">GTM IMPROVEMENT FOCUS</a></li>
+        <li className="Sidebar__menu-item"><a href="#OPERATIONS-CUSTOMER SPECIFIC">OPERATIONS-CUSTOMER SPECIFIC</a></li>
+        <li className="Sidebar__menu-item"><a href="#OPERATIONS-HCLTECH SPECIFIC">OPERATIONS-HCLTECH SPECIFIC</a></li>
+        <li className="Sidebar__menu-item"><a href="#CUSTOMER DELIGHT">CUSTOMER DELIGHT</a></li>
+        <li className="Sidebar__menu-item"><a href="#DEX">DEX</a></li>
+        <li className="Sidebar__menu-item"><a href="#ENGINEER DELIGHT">ENGINEER DELIGHT</a></li>
+        <li className="Sidebar__menu-item"><a href="#ENGINEER UPSKILLING">ENGINEER UPSKILLING</a></li>
+        <li className="Sidebar__menu-item"><a href="#GOVERNANCE-CUSTOMER FACING">GOVERNANCE-CUSTOMER FACING</a></li>
+        <li className="Sidebar__menu-item"><a href="#GOVERNANCE-INTERNAL FACING">GOVERNANCE-INTERNAL FACING</a></li>
       </ul>
-    </div>
+    </nav>
   );
 }
 
