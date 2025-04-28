c3bd6c079d46c8fe699eb9e45307ff7a329bd0b3
BrahamjeetRandhawa <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-04-28 11:14:54 +0200

    Main added

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Top from './Top'; // Import the Top component
 import Sidebar from './Sidebar'; // Import the Sidebar component
 import Filter from './Filter'; // Import the Filter component
+import Main from './Main'; // Import the Main component
 
 function App() {
   return (
@@ -12,10 +13,12 @@ function App() {
           <Top className="Topbar" />
         </header>
         <main className="Main">
-        <div className="Main-header">
+        <div className="Main-dashboard">
           <Filter className="Filter" />
           <Sidebar className="Sidebar" />
+          <Main className="Main-content" />
         </div>
+        
         </main>
       </body>
   );
---
+++ dashboard/src/Main.js
@@ -0,0 +1,14 @@
+import './Mainstyle.css';
+
+function Main() {
+    return (
+        <div className ="Main">
+            <h2>FINANCIALS</h2>
+
+            <h3>Revenue</h3>
+        </div>
+    );
+}
+
+
+export default Main;
