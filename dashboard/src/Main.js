fde164ab40181f7a84240d78a0ef542a87097e2e
BrahamjeetRandhawa <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-04-28 11:46:04 +0200

    Update

--- dashboard/src/Main.js
+++ dashboard/src/Main.js
@@ -2,10 +2,15 @@ import './Mainstyle.css';
 
 function Main() {
     return (
-        <div className ="Main">
+        <div className ="Main-style">
             <h2>FINANCIALS</h2>
-
             <h3>Revenue</h3>
+            <div className="Budget">
+                <div className="Cicle">
+                    <h4>26</h4>
+                </div>
+                <p>Budget</p>
+            </div>
         </div>
     );
 }
