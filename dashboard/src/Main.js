90360b359e37387836a9e1b49c8a8b885297be1f
BrahamjeetRandhawa <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-04-28 12:30:12 +0200

    Main dashboard template made

--- dashboard/src/Main.js
+++ dashboard/src/Main.js
@@ -1,19 +1,77 @@
-import './Mainstyle.css';
+import "./Mainstyle.css";
 
 function Main() {
-    return (
-        <div className ="Main-style">
-            <h2>FINANCIALS</h2>
-            <h3>Revenue</h3>
-            <div className="Budget">
-                <div className="Cicle">
-                    <h4>26</h4>
-                </div>
-                <p>Budget</p>
-            </div>
+  return (
+    <div className="Main-style">
+      <h2>FINANCIALS</h2>
+      <h3>Revenue</h3>
+      <div className="Boxes">
+        <div className="Box">
+          <div className="Circle">
+            <h4>26</h4>
+          </div>
+          <p>Budget</p>
         </div>
-    );
-}
+        <div className="Box">
+          <div className="Circle">
+            <h4>28</h4>
+          </div>
+          <p>Actuals</p>
+        </div>
+        <div className="Box">
+          <div className="Circle">
+            <h4>108</h4>
+          </div>
+          <p>Achieved percentage</p>
+        </div>
 
+        <span className="line"></span>
+
+
+
+        <div className="Box">
+          <div className="Circle">
+            <h4>28</h4>
+          </div>
+          <p>Budget</p>
+        </div>
+        <div className="Box">
+          <div className="Circle">
+            <h4>28</h4>
+          </div>
+          <p>Actuals</p>
+        </div>
+        <div className="Box">
+          <div className="Circle">
+            <h4>108</h4>
+          </div>
+          <p>Achieved percentage</p>
+        </div>
+        
+        <span className="line"></span>
+
+
+        <div className="Box">
+          <div className="Circle">
+            <h4>26%</h4>
+          </div>
+          <p>Budget</p>
+          </div>
+        <div className="Box">
+          <div className="Circle">
+            <h4>28%</h4>
+          </div>
+          <p>Actuals</p>
+          </div>
+        <div className="Box">
+          <div className="Circle">
+            <h4>108%</h4>
+          </div>
+          <p>Achieved percentage</p>
+          </div>
+      </div>
+    </div>
+  );
+}
 
 export default Main;
