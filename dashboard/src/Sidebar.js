f02ec4e3ee99642286bbb0e68f437506f0b351ef
Braham <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-05-26 16:03:02 +0200

    Sidebar layout updated.
    Animation added.
    
    Layout not yet correct

--- dashboard/src/Sidebar.js
+++ dashboard/src/Sidebar.js
@@ -1,6 +1,6 @@
 import './Sidebarstyle.css';
 
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { NavLink } from 'react-router-dom'; // Import Link from react-router-dom
 
 
 
@@ -8,21 +8,21 @@ function Sidebar() {
   return (
     <nav className="Sidebar">
       <ul className="Sidebar_menu">
-        <li className="Sidebar__menu-item"><Link to="/">FINANCIALS</Link></li>
-        <li className="Sidebar__menu-item"><Link to="/demand">DEMAND VIEW</Link></li>
-        <li className="Sidebar__menu-item"><Link to="/fulfilment">FULFILMENT VIEW</Link></li>
-        <li className="Sidebar__menu-item"><a href="#CLIENT PARTNER FOCUS">CLIENT PARTNER FOCUS</a></li>
-        <li className="Sidebar__menu-item"><Link to="/thought_Leadership">THOUGHT LEADERSHIP DEMONSTRATION</Link></li>
-        <li className="Sidebar__menu-item"><Link to="/north_Star">NORTH STAR POD</Link></li>
-        <li className="Sidebar__menu-item"><Link to="/GTM_improvement">GTM IMPROVEMENT FOCUS</Link></li>
-        <li className="Sidebar__menu-item"><a href="#OPERATIONS-CUSTOMER SPECIFIC">OPERATIONS-CUSTOMER SPECIFIC</a></li>
-        <li className="Sidebar__menu-item"><Link to="/operations_Hcltech">OPERATIONS - HCLTECH SPECIFIC</Link></li>
-        <li className="Sidebar__menu-item"><Link to="/customer_Delight">CUSTOMER DELIGHT</Link></li>
-        <li className="Sidebar__menu-item"><a href="#DEX">DEX</a></li>
-        <li className="Sidebar__menu-item"><Link to="/engineer_Delight">ENGINEER DELIGHT</Link></li>
-        <li className="Sidebar__menu-item"><Link to="/engineer_Upskilling">ENGINEER UPSKILLING</Link></li>
-        <li className="Sidebar__menu-item"><Link to="/governance_Customer">GOVERNANCE - CUSTOMER FACING</Link></li>
-        <li className="Sidebar__menu-item"><a href="#GOVERNANCE-INTERNAL FACING">GOVERNANCE-INTERNAL FACING</a></li>
+        <li className="Sidebar_menu-item"><NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}><span className='Sidebar_menu-indicator' />FINANCIALS</NavLink></li>
+        <li className="Sidebar_menu-item"><NavLink to="/demand" className={({ isActive }) => isActive ? "active-link" : ""}><span className='Sidebar_menu-indicator' />DEMAND VIEW</NavLink></li>
+        <li className="Sidebar_menu-item"><NavLink to="/fulfilment" className={({ isActive }) => isActive ? "active-link" : ""}><span className='Sidebar_menu-indicator' /> FULFILMENT VIEW</NavLink></li>
+        <li className="Sidebar_menu-item"><a href="#CLIENT PARTNER FOCUS" className={({ isActive }) => isActive ? "active-link" : ""}><span className='Sidebar_menu-indicator' /> CLIENT PARTNER FOCUS</a></li>
+        <li className="Sidebar_menu-item"><NavLink to="/thought_Leadership" className={({ isActive }) => isActive ? "active-link" : ""}><span className='Sidebar_menu-indicator' /> THOUGHT LEADERSHIP DEMONSTRATION</NavLink></li>
+        <li className="Sidebar_menu-item"><NavLink to="/north_Star">NORTH STAR POD</NavLink></li>
+        <li className="Sidebar_menu-item"><NavLink to="/GTM_improvement">GTM IMPROVEMENT FOCUS</NavLink></li>
+        <li className="Sidebar_menu-item"><a href="#OPERATIONS-CUSTOMER SPECIFIC">OPERATIONS-CUSTOMER SPECIFIC</a></li>
+        <li className="Sidebar_menu-item"><NavLink to="/operations_Hcltech">OPERATIONS - HCLTECH SPECIFIC</NavLink></li>
+        <li className="Sidebar_menu-item"><NavLink to="/customer_Delight">CUSTOMER DELIGHT</NavLink></li>
+        <li className="Sidebar_menu-item"><a href="#DEX">DEX</a></li>
+        <li className="Sidebar_menu-item"><NavLink to="/engineer_Delight">ENGINEER DELIGHT</NavLink></li>
+        <li className="Sidebar_menu-item"><NavLink to="/engineer_Upskilling">ENGINEER UPSKILLING</NavLink></li>
+        <li className="Sidebar_menu-item"><NavLink to="/governance_Customer">GOVERNANCE - CUSTOMER FACING</NavLink></li>
+        <li className="Sidebar_menu-item"><a href="#GOVERNANCE-INTERNAL FACING">GOVERNANCE-INTERNAL FACING</a></li>
       </ul>
     </nav>
   );
