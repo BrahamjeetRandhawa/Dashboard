db492e2754bf959535a2d36aa76a95d969f0cee0
Braham <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-05-29 14:48:23 +0200

    Circle update.
    Fix the alignment of Main2

--- dashboard/src/Main2.js
+++ dashboard/src/Main2.js
@@ -1,5 +1,7 @@
 
 import "./Mainstyle2.css";
+import CircleAnimation from "./circleAnimation";
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -40,13 +42,7 @@ function Main2() {
           
         <div className={`Box2 Box2-id-${item.id}`} key={item.id}>
           
-          <div className="Circle2"
-          style={{
-            "--color": color(item.percentage),
-            "--target-percentage": `${item.percentage * 3.6}deg`
-        }}>
-            <h4>{item.percentage}</h4>
-          </div>
+          <CircleAnimation percentage={item.percentage} />
           <div className="Text-container2">
           <h3 className="Main2-h3">{item.title}</h3>
           <p className="Label2">{item.label}</p>
