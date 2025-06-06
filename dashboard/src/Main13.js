95cdeb09f6492f252506dfdde7cfe055f7407962
Braham <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-06-06 12:33:01 +0200

    Small change

--- dashboard/src/Main13.js
+++ dashboard/src/Main13.js
@@ -35,11 +35,7 @@ function Main13() {
         </div>
 
         {groupedData.map((group, groupIndex) => {
-          const color = (percentage) => {
-          if (percentage >= 100) return "#00cc66";
-          if (percentage >= 95) return "#ffcc00";
-          return "#ff0000";
-        };
+          
          return (
         <Fragment key={groupIndex}>
           
